f7b461e7671d63d820a17ae6c1133bd7c8f7137e
jiak1 <jackdonaldson005@gmail.com>
2020-04-28 01:25:48 +0000

    Google Analytics

--- maincli.py
+++ maincli.py
@@ -1,8 +1,8 @@
-from flask import Flask,redirect
+#from flask import Flask,redirect
 #This is for server-lists.com and www.server-lists.com
 
-mainProgram = Flask(__name__,static_url_path="", static_folder="static")
+#mainProgram = Flask(__name__,static_url_path="", static_folder="static")
 
-@mainProgram.route("/")
-def mainRoute():
-	return redirect("https://minecraft.server-lists.com")
+#@mainProgram.route("/")
+#def mainRoute():
+#	return redirect("https://minecraft.server-lists.com")
