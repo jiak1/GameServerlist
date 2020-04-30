2304d7354209181756bd194e38e854f7fde72e0a
jiak1 <jackdonaldson005@gmail.com>
2020-04-30 08:03:04 +0000

    Loggin now fixed

--- application/ErrorHandler.py
+++ application/ErrorHandler.py
@@ -1,11 +1,11 @@
 from .MCRoutes import MCRoutes
 from flask import render_template
 from .Program import mc_app as app
+import traceback
 
-@MCRoutes.errorhandler(500)
+@MCRoutes.errorhandler(Exception)
 def internal_error(error):
-	return render_template('mc/error.html'),500
-
-@MCRoutes.errorhandler(404)
-def not_found_error(error):
-	return render_template('mc/error.html'),404
+	result = traceback.format_exc()
+	with open("logging.txt", "a") as f:
+		f.write(result)
+	return render_template('mc/error.html')
