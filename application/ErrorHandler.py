723897a0401fd989f197e12ef4567c3ddd0237dc
jiak1 <jackdonaldson005@gmail.com>
2020-04-30 08:25:45 +0000

    Errors may work..

--- application/ErrorHandler.py
+++ application/ErrorHandler.py
@@ -1,10 +1,19 @@
 from .MCRoutes import MCRoutes
 from flask import render_template
-from .Program import mc_app as app
 import traceback
 
-@MCRoutes.errorhandler(Exception)
+@MCRoutes.errorhandler(500)
 def internal_error(error):
+	try:
+		result = traceback.format_exc()
+		with open("logging.txt", "a") as f:
+			f.write(result)
+	except:
+		pass
+	return render_template('mc/error.html')
+
+@MCRoutes.errorhandler(404)
+def not_found_error(error):
 	try:
 		result = traceback.format_exc()
 		with open("logging.txt", "a") as f:
