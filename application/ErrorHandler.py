605a06be54cda82755b3545cc6af7349769e0ba9
jiak1 <jackdonaldson005@gmail.com>
2020-05-07 07:51:03 +0000

    error page

--- application/ErrorHandler.py
+++ application/ErrorHandler.py
@@ -1,6 +1,6 @@
 from .MCRoutes import MCRoutes
 from flask import render_template
-import traceback
+from sentry_sdk import last_event_id
 
 #@MCRoutes.errorhandler(500)
 #def internal_error(error):
@@ -9,3 +9,7 @@ import traceback
 #@MCRoutes.errorhandler(404)
 #def not_found_error(error):
 #	return render_template('mc/error.html')
+
+@MCRoutes.errorhandler(500)
+def server_error_handler(error):
+    return render_template("mc/error.html", sentry_event_id=last_event_id()), 500
