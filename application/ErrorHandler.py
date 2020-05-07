f13a51e8da2d56baf2574cfa5cf27f8d33d0fd29
jiak1 <jackdonaldson005@gmail.com>
2020-05-07 06:26:35 +0000

    fixed no favicon

--- application/ErrorHandler.py
+++ application/ErrorHandler.py
@@ -4,20 +4,8 @@ import traceback
 
 @MCRoutes.errorhandler(500)
 def internal_error(error):
-	try:
-		result = traceback.format_exc()
-		with open("logging.txt", "a") as f:
-			f.write(result)
-	except:
-		pass
 	return render_template('mc/error.html')
 
 @MCRoutes.errorhandler(404)
 def not_found_error(error):
-	try:
-		result = traceback.format_exc()
-		with open("logging.txt", "a") as f:
-			f.write(result)
-	except:
-		pass
 	return render_template('mc/error.html')
