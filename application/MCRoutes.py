616da7f82fdfe3f02940344afe73b9bff6d91ed7
jiak1 <jackdonaldson005@gmail.com>
2020-05-22 05:55:06 +0000

    Fixed elasticsearch

--- application/MCRoutes.py
+++ application/MCRoutes.py
@@ -49,7 +49,7 @@ def MCHomePage():
 			canonURL="https://minecraft.server-lists.com?search="+search
 			
 		return render_template("mc/index.html",servers=servers,search=search,next_url=next_url,prev_url=prev_url,cacheNum=SUGGESTION_CACHE_NUM,title="",description="Find the best Minecraft Servers using our Minecraft Server List. Find a server that suits your needs with hundreds of categories!",canonURL=canonURL)
-	except:
+	except TypeError as e:
 		#runs if we go to an invalid page
 		return redirect(url_for("MCRoutes.MCHomePage",search=search))
 
